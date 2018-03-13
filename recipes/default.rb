12456fd0036e20f658f50c5fdbfdc39ef78ba9b4
Fred Thompson <fred.thompson@buildempire.co.uk>
2018-03-13 08:07:54 +0000

    Adding logrotate, papertrail, efs and directory creation into the cookbook too, to allow a high level of monitoring along with the main server.

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,5 +5,9 @@
 
 include_recipe 'apt'
 include_recipe 'appbox'
+include_recipe 'cookbook_clarus_worker::directories'
 include_recipe 'cookbook_clarus_worker::ruby'
 include_recipe 'cookbook_clarus_worker::postgresql'
+include_recipe 'cookbook_clarus_worker::efs'
+include_recipe 'cookbook_clarus_worker::papertrail'
+include_recipe 'cookbook_clarus_worker::logrotate'
