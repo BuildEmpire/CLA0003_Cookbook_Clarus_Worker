ac4878d4a001bf5b9c3122660fc6164e1143f374
Fred Thompson <fred.thompson@buildempire.co.uk>
2018-03-13 07:30:49 +0000

    CLA0003-933 - Unicorn To Passenger
    Switched to using a postgresql script so we can use the new resource method of installing the postgresql client, rather than the recipe.
    Additionally, i've added the system wide postgres gem, as if we ever want to FTP to a work, the FTP auth script needs this.
    
    #CLA0003-933 Fixed

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,6 @@
 #
 
 include_recipe 'apt'
-include_recipe 'postgresql::client'
 include_recipe 'appbox'
 include_recipe 'cookbook_clarus_worker::ruby'
+include_recipe 'cookbook_clarus_worker::postgresql'
