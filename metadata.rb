30ffe4a4916e422127bd2229a8d2fd9965f00909
Fred Thompson <fred.thompson@buildempire.co.uk>
2018-04-29 10:19:02 +0100

    CLA0003-970 - Data Import On Demand
    Didn't get the code right last time! :(

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'fred.thompson@clarussoftware.co.uk'
 license          'Apache 2.0'
 description      'The Clarus worker cookbook, running resque.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.5'
+version          '0.0.6'
 
 recipe 'cookbook_clarus_worker', 'The Clarus worker cookbook, running resque.'
 
